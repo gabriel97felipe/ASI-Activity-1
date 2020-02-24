7e9d32e5cd0eb4e4b1497f39657f73c69765826e
Gabriel Felipe <gabriel97_felipe@hotmail.com>
2020-02-24 20:38:10 +0000

    Commit of the API

---
+++ API-Template/API-Template/Controllers/WeatherForecastController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace API_Template.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("{zipCode}")]
+        public WeatherForecast Get(int zipCode)
+        {
+
+
+            var dateString = "02/24/2020 07:00 PM";
+            var culture = CultureInfo.CreateSpecificCulture("en-US");
+            var styles = DateTimeStyles.None;
+            var rng = new Random();
+            var rInt = rng.Next(-20, 50);
+            return new WeatherForecast
+            {
+                Date = DateTime.Parse(dateString, culture, styles),
+                TemperatureC = rInt,
+                Summary = "Sunny"
+            };
+        }
+
+        public WeatherForecast Get()
+        {
+            var dateString = "02/24/2020 07:00 PM";
+            var culture = CultureInfo.CreateSpecificCulture("en-US");
+            var styles = DateTimeStyles.None;
+            var rng = new Random();
+            return new WeatherForecast
+            {
+                Date = DateTime.Parse(dateString, culture, styles),
+                TemperatureC = 14,
+                Summary = "Sunny"
+            };
+        }
+    }
+}
